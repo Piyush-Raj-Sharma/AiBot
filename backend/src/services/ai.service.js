36a8a8761a0e2303af27a0b8fd567a73f710659a
Piyush Raj <112124716+Piyush-Raj-Sharma@users.noreply.github.com>
2025-10-22 19:49:27 +0530

    Update ai.service.js

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -9,7 +9,7 @@ async function generateResponse(chat_history) {
   
   try {
     const response = await ai.models.generateContent({
-      model: "gemini-1.5-flash",
+      model: "gemini-2.0-flash",
       contents: chat_history,
       config: {
         systemInstruction:
